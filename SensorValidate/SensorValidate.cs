6261e58dc2029f1a08570b5361856b26100cdc17
Abirami <77603661+Abirami-Sivamani@users.noreply.github.com>
2021-03-23 09:44:41 +0530

    Update SensorValidate.cs

--- SensorValidate/SensorValidate.cs
+++ SensorValidate/SensorValidate.cs
@@ -5,29 +5,32 @@ namespace SensorValidate
 {
     public class SensorValidator
     {
-        public static bool _give_me_a_good_name(double value, double nextValue, double maxDelta) {
+        public static bool IsHigherThanMaxDelta(double value, double nextValue, double maxDelta) {
             if(nextValue - value > maxDelta) {
                 return false;
             }
             return true;
         }
-        public static bool validateSOCreadings(List<Double> values) {
+        
+        public static bool ValidateSOCreadings(List<Double> values) {
+            return ValidateReadings(values, 0.05);
+        }
+        
+        public static bool ValidateCurrentReadings(List<Double> values) {
+            return ValidateReadings(values, 0.1);
+        }
+
+        static bool ValidateReadings(List<Double> values, double maxDelta)
+        {
             int lastButOneIndex = values.Count - 1;
-            for(int i = 0; i < lastButOneIndex; i++) {
-                if(!_give_me_a_good_name(values[i], values[i + 1], 0.05)) {
+            for (int i = 0; i < lastButOneIndex; i++)
+            {
+                if (!IsHigherThanMaxDelta(values[i], values[i + 1], maxDelta))
+                {
                     return false;
                 }
             }
             return true;
-        }
-        public static bool validateCurrentreadings(List<Double> values) {
-            int lastButOneIndex = values.Count - 1;
-            for(int i = 0; i < lastButOneIndex; i++) {
-                if(!_give_me_a_good_name(values[i], values[i + 1], 0.1)) {
-                    return false;
-                }
-            }
-            return true;
-        }
+        } 
     }
 }

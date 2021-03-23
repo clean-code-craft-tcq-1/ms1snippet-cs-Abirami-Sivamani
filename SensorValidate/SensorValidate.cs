c8cbebaddfeeec150d213ec6f413106ea91a2c67
Abirami <77603661+Abirami-Sivamani@users.noreply.github.com>
2021-03-23 18:27:35 +0530

    Update SensorValidate.cs

--- SensorValidate/SensorValidate.cs
+++ SensorValidate/SensorValidate.cs
@@ -13,28 +13,34 @@ namespace SensorValidate
         }
         
         public static bool ValidateSOCreadings(List<Double> values) {
-            return ValidateReadings(values, 0.05);
+            if(IsReadingsEmpty(values))
+                return ValidateReadings(values, 0.05);
+            return false;
         }
-        
         public static bool ValidateCurrentReadings(List<Double> values) {
-            return ValidateReadings(values, 0.1);
+            if (IsReadingsEmpty(values))
+                return ValidateReadings(values, 0.1);
+            return false;
         }
 
         static bool ValidateReadings(List<Double> values, double maxDelta)
         {
             int lastButOneIndex = values.Count - 1;
-            if (values.Count > 0)
+            for (int i = 0; i < lastButOneIndex; i++)
             {
-                for (int i = 0; i < lastButOneIndex; i++)
+                if (!IsHigherThanMaxDelta(values[i], values[i + 1], maxDelta))
                 {
-                    if (!IsHigherThanMaxDelta(values[i], values[i + 1], maxDelta))
-                    {
-                        return false;
-                    }
+                    return false;
                 }
-                return true;
             }
+            return true;
+        }
+        
+        static bool IsReadingsEmpty(List<Double> values)
+        {
+            if (values.Count > 0)
+                return true;
             return false;
-        } 
+        }
     }
 }
